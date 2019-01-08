5ea3fe0dddd5cc9307d8a09de14912584d912df8
abhimanyu891998 <singhabhimanyu186@gmail.com>
2019-01-08 14:55:24 +0800

    Full functionality, Incomplete CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ResultComponent from './components/ResultComponent';
+import KeyPadComponent from './components/KeyPadComponent';
+
 
 class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      result: ""
+    }
+  }
+
+
+  calculate = () => {
+    try {
+        this.setState({
+            // eslint-disable-next-line
+            result: (eval(this.state.result) || "" ) + ""
+        })
+    } catch (e) {
+        this.setState({
+            result: "error"
+        })
+
+    }
+};
+
+  reset = () => {
+      this.setState({
+          result: ""
+      })
+  };
+
+  backspace = () => {
+      this.setState({
+          result: this.state.result.slice(0, -1)
+      })
+  };
+
+  onClick = button => {
+
+    if(button === "="){
+        this.calculate()
+    }
+
+    else if(button === "C"){
+        this.reset()
+    }
+    else if(button === "CE"){
+        this.backspace()
+    }
+
+    else {
+        this.setState({
+            result: this.state.result + button
+        })
+    }
+};
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="calculator-body">
+        <h1>Coolest calculator ever made ft. Crappy CSS</h1>
+        <ResultComponent result={this.state.result} />
+        <KeyPadComponent onClick={this.onClick} />
       </div>
     );
   }
